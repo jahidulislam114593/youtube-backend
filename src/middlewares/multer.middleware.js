3ef19283ba37db1b7bab06743db617ddbd76f7a4
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-11-05 22:36:57 +0600

    [debug ENOERT]

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,28 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "../../public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({ storage });
+
+//------For Solving Error: ENOENT: no such file or directory, open 'E:\WebProject\JSBackend\public\temp\profile-pic.png'-------
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// If using ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../public/temp");
+    cb(null, path.join(__dirname, "../../public/temp"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
